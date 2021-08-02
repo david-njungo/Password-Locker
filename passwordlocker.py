8fef3ad246bf05244576c22961469a97f2cc0aac
David Njungo <davidnjungo3@gmail.com>
2021-08-02 11:53:02 +0300

    add the user class

---
+++ passwordlocker.py
@@ -0,0 +1,20 @@
+class User:
+    """
+    Class that generates new instances of User.
+    """
+
+    user_details = [] # Empty contact list
+
+    def __init__(self,first_name,last_name,password,username):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
+        self.username = username
+
+    # def save_userdetails(self):
+
+    #     '''
+    #     save_contact method saves user's  into user_details
+    #     '''
+
+    #     User.user_details.append(self)
