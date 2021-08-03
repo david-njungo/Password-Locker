3fa1fc7b7c3727eea0b73da11b37a11324e61d12
David Njungo <davidnjungo3@gmail.com>
2021-08-03 06:17:39 +0300

    add the run.py file

---
+++ run.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3.8
+from passwordlocker import User
+def create_account(firstname,lastname,username,password):
+        '''
+        Function to create a new User
+        '''
+        new_user = User(firstname,lastname,username,password)
+        return new_user
+
+def save_details(user):
+        '''
+        Function to save userdetails
+        '''
+        user.save_userdetails()
+
+
+def main():
+        print("*"*80)
+        print("                      PASSWORD LOCKER")
+        print("""
+                Hi,welcome to the password locker account.
+                Save all your credentials here!
+                """)
+        print("What would you like to do?")       
+        while True:
+                print("Use these short codes : ua - create a new user account, la - login account")
+                short_code = input().lower()
+                if short_code == "ua":
+                                print("Sign in account")
+                                print("-"*80)   
+                                print("First name ....")
+                                first_name=input(">")   
+                                print("Last name .....")
+                                last_name=input(">")    
+                                print("Enter username")
+                                username=input(">")    
+                                print("Enter password")
+                                password=input(">")     
+                                save_details(create_account(first_name,last_name,username,password))        
+                                print(f"{username} you have successfully created an account proceed to login")     
+
+
+if __name__ == '__main__':
+
+    main()
